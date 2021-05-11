5c2f25ee8bd7f514aa0b09ddbd0129900af09031
Christian Gustavo Ribeiro Gomes <christiangomes00@gmail.com>
2021-05-10 22:13:17 -0300

    Refactoring header component and adding signup button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+
+  html!: HTMLElement;
+
+  constructor(
+    private renderer: Renderer2,
+  ) {}
+
+  ngOnInit(): void {
+    this.selectHTMLElement();
+  }
+
+  private selectHTMLElement(): void {
+    this.html = this.renderer.selectRootElement('html', true);
+  }
+
+  toggleDarkMode(): void {
+    this.html.classList.toggle('dark');
+  }
+
+  signUp(): void {
+
+  }
+
+}
